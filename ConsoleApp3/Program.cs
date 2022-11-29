296b0f357cd490cec83a56b06a1a2ceb0b060f4e
ibramila <70vq7w9@code.edu.az>
2022-11-29 12:54:22 +0400

    foo created

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿int FindElement<T>(T [] arr,CheckNumber<T> ch)
+{
+	for (int i = 0; i < arr.Length; i++)
+	{
+		if (ch(arr[i]))
+		{
+			return i;
+		}
+	}
+    return -1;
+}
+
+bool IsZero(int element)
+{
+	if(element == 0)
+	{
+		return true;
+	}
+	return false;
+}
+
+Console.WriteLine(FindElement(new int[] {-1,2,0,3,0}, IsZero));
+
+delegate bool CheckNumber<K>(K element);
